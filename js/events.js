05d79ab6f35da18f8c924077c055ddbefb5f4ca5
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 20:04:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $("p").on('click', function(key) {
+    alert('Hey!');
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
 
-// call functions here
-
+function pressIt() {
+$("form").on('keydown', function(key) {
+  if(key.which == 71){
+      alert('alert the user');
+  }
 });
+
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  }
+);}
+
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+})
